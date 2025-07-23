3f12161c74734b88eb117b2a9c252d93e71de54f
User <nimthecloud05@gmail.com>
2025-07-23 17:25:56 +0300

    fixed the navigation

--- App.js
+++ App.js
@@ -17,13 +17,12 @@ import SettingsScreen from './screens/SettingsScreen'
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function MainTabs() {
-  return (
-    <Stack.Navigator initialRouteName='Explore'>
-      <Stack.Screen name='Profile' component={ProfileScreen} />
-      <Stack.Screen name='Explore' component={ExploreScreen} />
-      <Stack.Screen name='Settings' component={SettingsScreen} />
-    </Stack.Navigator>
+function MainTabs(){
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name='Explore' component={ExploreScreen} />
+      <Tab.Screen name='Profile' component={ProfileScreen} />
+    </Tab.Navigator>
   )
 }
 
@@ -32,6 +31,7 @@ function StartStack() {
     <Stack.Navigator initialRouteName='Login'>
       <Stack.Screen name='Create Profile' component={CreateProfile} options={{ headerShown: false }} />
       <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='Tabs' component={MainTabs} options={{ headerShown: false }} />
       <Stack.Screen name='Create Group' component={CreateGroup} options={{ headerShown: false }} />
     </Stack.Navigator>
   )
@@ -39,7 +39,16 @@ function StartStack() {
 
 function SettingsStack() {
   return (
-    <Tab.Navigator initialRouteName='Settings'>
+    <Stack.Navigator initialRouteName='Profile'>
+      <Stack.Screen name='Profile' component={ProfileScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='Settings' component={SettingsScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  )
+}
+
+function SettingsStack() {
+  return (
+    <Tab.Navigator initialRouteName='Profile'>
       <Tab.Screen name='Settings' component={SettingsScreen} />
     </Tab.Navigator>
   )
@@ -52,7 +61,6 @@ export default function App() {
         <Stack.Screen name='StartStack' component={StartStack} />
         <Stack.Screen name='MainTabs' component={MainTabs} />
         <Stack.Screen name='Profile' component={ProfileScreen} />
-        <Stack.Screen name='Explore' component={ExploreScreen} />
         <Stack.Screen name='Settings' component={SettingsScreen} />
         {/* <Stack.Screen name='Settings' component={SettingsStack} /> */}
       </Stack.Navigator>
