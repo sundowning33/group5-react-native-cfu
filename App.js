635f2563a61cdb2ae95baf499104d0a0612ed6c8
Mariia <mariiasemerei@gmail.com>
2025-07-24 14:17:36 +0300

    Update App.js to show icons

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
@@ -16,19 +17,45 @@ import ExploreScreen from './screens/ExploreScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import SettingsScreen from './screens/SettingsScreen'
 
-// import {styles} from "styles.js"
-
 const Stack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
 
-function MainTabs (){
-  return(
-    <Tabs.Navigator>
-      <Tabs.Screen name="Explore" component={ExploreScreen} options={{headerShown: false}} />
-      <Tabs.Screen name="Profile" component={ProfileScreen} />
-      <Tabs.Screen name="Settings" component={SettingsScreen} />
+function MainTabs() {
+  return (
+    <Tabs.Navigator screenOptions={{
+      tabBarActiveTintColor: '#C63B62',
+      tabBarInactiveTintColor: '#5A3353',
+    }}>
+      <Tabs.Screen name="Explore" component={ExploreScreen} options={{
+        tabBarIcon: ({ color }) => {
+          return <Ionicons name='home' size={20} color={color} />;
+        }
+      }} />
+      <Tabs.Screen name="Profile" component={ProfileScreen} options={{
+        tabBarIcon: ({ color }) => {
+          return <Ionicons name='person' size={20} color={color} />;
+        }
+      }} />
+      <Tabs.Screen name="Settings" component={SettingsScreen} options={{
+        tabBarIcon: ({ color }) => {
+          return <Ionicons name='settings' size={20} color={color} />;
+        }, headerShown: false
+      }} />
     </Tabs.Navigator>
-  )
+  );
+}
+
+export default function App() {        
+  return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login" >
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Create Group" component={CreateGroup} />
+            <Stack.Screen name="Create Account" component={CreateProfile} />
+            <Stack.Screen name="Tabs" component={MainTabs} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        );
 }
 
 export default function App() {
